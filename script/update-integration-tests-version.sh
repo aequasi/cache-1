4aef46b31481c04d22def17ce1164e75bb076575
Aaron Scherer <aequasi@gmail.com>
2016-02-02 15:17:01 -0800

    Exluding 7.0 for apc

--- script/update-integration-tests-version.sh
+++ script/update-integration-tests-version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ $# -eq 0 ]
-  then
+then
     echo "No arguments supplied. You need to specify the version to composer."
     exit 1
 fi
@@ -11,21 +11,21 @@ ROOT=$(pwd)
 
 # Run for each components
 find src -mindepth 2 -type f -name phpunit.xml.dist | while read line; do
- # Save the directory name
- DIR=$(dirname $line)
+   # Save the directory name
+   DIR=$(dirname $line)
 
- # Go to that directory
- cd $DIR
- pwd
+   # Go to that directory
+   cd $DIR
+   pwd
 
- # Let composer update the composer.json
- composer require --dev --no-update cache/integration-test:$VERSION
+   # Let composer update the composer.json
+   composer require --dev --no-update cache/integration-tests:$VERSION
 
- # Go back to the root
- cd $ROOT
- echo ""
+   # Go back to the root
+   cd $ROOT
+   echo ""
 done
 
 # Update integration test for the root
 pwd
-composer require --dev --no-update cache/integration-test:$VERSION
+composer require --dev --no-update cache/integration-tests:$VERSION
