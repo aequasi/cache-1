fe5132f8559b4b4070fc20dc01f84c04b987c831
Aaron Scherer <aequasi@gmail.com>
2016-02-02 17:11:45 -0800

    Updating builds!

--- build/setEnvVariables.sh
+++ build/setEnvVariables.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-if [ ! -z "$ADAPTER" ]; then export PROJECT=$ADAPTER; export DIR=src/Adapter;  fi
-if [ ! -z "$LIBRARY" ]; then export PROJECT=$LIBRARY; export DIR=src;  fi
+if [ ! -z "$BUILD_ALL" ]
+then
+    export PROJECT=""
+    export DIR="."
+else
+    if [ ! -z "$ADAPTER" ]; then export PROJECT=$ADAPTER; export DIR=src/Adapter;  fi
+    if [ ! -z "$LIBRARY" ]; then export PROJECT=$LIBRARY; export DIR=src;  fi
 
-if [ $ADAPTER == 'MongoDB' ]
+fi
+
+if [[ $ADAPTER == 'MongoDB' || ! -z "$BUILD_ALL" ]]
 then
     export MONGODB_HOST="localhost:27017"
     export MONGODB_DATABASE="test"
