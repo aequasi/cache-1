7f407c6c54730c9e4b35c46f52de1be7df4b754a
Aaron Scherer <aequasi@gmail.com>
2016-02-02 16:35:18 -0800

    Updating build scripts

--- build/php/5.5/MongoDB.sh
+++ build/php/5.5/MongoDB.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Disable xdebug"
-phpenv config-rm xdebug.ini
-
-echo "Add php.ini settings"
-phpenv config-add ./build/php/php.ini
-
-echo "Install codecov"
-pip install --user codecov
-
 echo "Install Mongo"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
@@ -18,11 +9,6 @@ sudo apt-get install mongodb-enterprise
 sudo apt-get -y install gdb
 
 if ! nc -z localhost 27017; then sudo service mongod start; fi
-mongod --version
-pecl install -f mongodb-1.1.2
-mongo --eval 'tojson(db.runCommand({buildInfo:1}))'
-php --ri mongodb
 
-echo "Enable extension"
-echo "extension = memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+pecl install -f mongodb-1.1.2
 
